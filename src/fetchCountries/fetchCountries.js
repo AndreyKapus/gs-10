962877c9ef56f82a73fd5363a6adcb922a662e24
AndreyKapus <kapustnikov@ukr.net>
2023-03-15 14:14:44 +0200

    countries

---
+++ src/fetchCountries/fetchCountries.js
@@ -0,0 +1,13 @@
+
+export function fetchCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+  .then((responce) => {
+    if(!responce.ok) {
+      throw new Error(responce.statusText)
+    }
+  return responce.json()
+  }).then(data => {
+    console.log(data)
+    return data;
+});
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+import {fetchCountries} from './fetchCountries/fetchCountries'
 
 const DEBOUNCE_DELAY = 300;
 
-function fetchCountries() {
-  fetch('https://restcountries.com/v3.1/name/deutschland').then((responce) => {
-    return responce.json();
-  }).then(data => {
-    console.log(data)
-    return data;
-});
+const inputEl = document.getElementById('search-box');
+const countryListEl = document.querySelector('.country-list')
+
+function renderCountries(e) {
+  e.preventDefault();
+ const countries = fetchCountries(e.target.value);
+  console.log(e.target.value);
+  if(countries) {
+    drawCountries(e.target.value)
+  }
+
+};
+
+function drawCountries(data) {
+  const markup = data.map(({name, capital, flag, languages, population}) => {
+    return `<h1>${name.oficial}</h1>
+    <li>${capital}</li>
+    `
+  }).join('')
+  countryListEl.insertAdjacentHTML('afterbegin', markup)
 }
-fetchCountries()
+
+inputEl.addEventListener('input', debounce(renderCountries, DEBOUNCE_DELAY))
+
+
